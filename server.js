13bdb037e6d2ba3d2f40471c39e71e8604b26766
alex v <alex@encrypt-s.com>
2018-07-05 09:57:10 +0000

    update address

--- server.js
+++ server.js
@@ -35,24 +35,30 @@ app.post('/', function (req, res) {
       } else {
         let r = JSON.parse(body);
         if(!req.body.signature) {
+	  console.log(r);
           if(r.length > 0) {
-            res.render('index', {step: 1, error: 'This username is already registered! Provide a cryptographic signature to update it.', address: req.body.address, email: req.body.email});
+            var prevAddress = r[0].value.split("oa1:nav recipient_address=")[1].split(";")[0];
+            if(!prevAddress) {
+              res.render('index', {step: 0, error: 'This username is already registered.'});
+            } else {
+              res.render('index', {step: 1, error: 'This username is already registered! Provide a cryptographic signature to update it.', prevaddress: prevAddress, address: req.body.address, email: req.body.email});
+            }
           } else {
-            res.render('index', {step: 1, error: '', address: req.body.address, email: req.body.email});
+            res.render('index', {step: 1, error: '', prevaddress: req.body.address, address: req.body.address, email: req.body.email});
           }
         } else {
           try {
-            var boolMessage = Message(req.body.email+'@nav.community').verify(req.body.address, req.body.signature);
+            var boolMessage = Message(req.body.email+'@nav.community').verify(req.body.prevaddress, req.body.signature);
           }catch(e){
-            res.render('index', {step: 1, error: 'Wrong signature', address: req.body.address, email: req.body.email});
+            res.render('index', {step: 1, error: 'Wrong signature', prevaddress: req.body.prevaddress, address: req.body.address, email: req.body.email});
             return;
           }
           if(!boolMessage) {
-            res.render('index', {step: 1, error: 'Wrong signature', address: req.body.address, email: req.body.email});
+            res.render('index', {step: 1, error: 'Wrong signature', prevaddress: req.body.prevaddress, address: req.body.address, email: req.body.email});
           } else {
             request({ url: util.format(config.url, req.body.email+".nav.community", "txt"), method: "POST", headers: { "Authorization": auth }, body: "oa1:nav recipient_address="+req.body.address+";" }, function (err, response, body) {
               if(err){
-                res.render('index', {step: 1, error: 'Internal error', address: req.body.address, email: req.body.email});
+                res.render('index', {step: 1, error: 'Internal error', prevaddress: req.body.address, address: req.body.address, email: req.body.email});
               } else {
                 res.render('index', {step: 2, error: '', address: req.body.address, email: req.body.email});
               }
